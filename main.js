cc257e8c571b17fb5902d82d024fe721573bef35
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-08-22 09:38:00 -0400

    Viewport responsiveness optimization for #home and #products pages

--- main.js
+++ main.js
@@ -1,12 +1,4 @@
 import "/style.css";
-import { setupCounter } from "./counter.js";
 import testimonials from "./javascripts/testimonials.js";
 
-document.querySelector("#app").innerHTML = `
-  <div>
-    <p>Default JS entrypoint</p>
-  </div>
-`;
-
-// setupCounter(document.querySelector("#counter"));
 testimonials(document.querySelector(".testimonials"));
